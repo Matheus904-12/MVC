52f6ab7dde31f9a2a920bca1603576c20da2e03e
Matheus Lucindo dos Santos <67837282+Matheus904-12@users.noreply.github.com>
2024-02-19 13:58:33 -0300

    .

--- ambiente.js
+++ ambiente.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const calculadoraController = require('../controller/executa.js');
+const calculadoraController = require('./controller/executa.js');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/view'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/view/index.html');
 });
-app.use('/calcular'. calculadoraController);
+app.use('/calcular', calculadoraController);
 const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
