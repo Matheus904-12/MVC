4063d821d664c1a58c94f327d001ef18853dacd1
Matheus Lucindo dos Santos <67837282+Matheus904-12@users.noreply.github.com>
2024-02-19 13:29:15 -0300

    .

---
+++ model/calculo.js
@@ -0,0 +1,10 @@
+
+class Calculadora {
+    static Hipotenusa(cateto1, cateto2) {
+        return Math.sqrt(cateto1 ** 2 + cateto2 ** 2);
+    }
+}
+
+module.exports = Calculadora;
+
+// Regra da operação
